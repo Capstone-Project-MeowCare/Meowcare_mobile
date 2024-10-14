c1f7475d8afec14e884a00e9f705395ad09d9ab2
Cassme1202 <NLDTan120201@gmail.com>
2024-10-15 02:49:21 +0700

    update

--- src/pages/Job/CatSitterGuide.jsx
+++ src/pages/Job/CatSitterGuide.jsx
@@ -1,23 +1,67 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
 
 export default function CatSitterGuide({navigation}) {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Rỗng</Text>
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <Image
+            source={require("../../../assets/BackArrow.png")}
+            style={styles.backArrow}
+          />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Hướng dẫn</Text>
+      </View>
+      {/* Đường kẻ ngang */}
+      <View style={styles.divider} />
+
+      {/* Nội dung khác */}
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#FFFAF5",
-    justifyContent: "center",
-    alignItems: "center",
   },
-  text: {
-    textAlign: "center",
-    fontSize: 20,
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    backgroundColor: "#FFF7F0", // Màu nền của header (tùy chỉnh theo yêu cầu)
+    justifyContent: "space-between", // Để căn đều các phần tử
+  },
+  backButton: {
+    padding: 8,
+  },
+  backArrow: {
+    width: 30,
+    height: 30,
+    tintColor: "#000080", // Màu sắc của mũi tên quay lại
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#1F1F1F", // Màu sắc tiêu đề
+    flex: 1,
+    textAlign: "center", // Để căn giữa tiêu đề
+  },
+  divider: {
+    borderBottomColor: "#D3D3D3", // Màu của đường kẻ ngang
+    borderBottomWidth: 1, // Độ dày của đường kẻ
+    marginTop: -8, // Điều chỉnh vị trí để sát với phần header hơn
   },
 });
